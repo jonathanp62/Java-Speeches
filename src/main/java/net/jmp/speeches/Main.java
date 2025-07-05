489573b8a131fc8948811241c56a66a4d64c88fb
Jonathan Parker <jonathanp62@gmail.com>
2025-07-05 09:32:12 -0400

    Initial 0.1.0 commit

---
+++ src/main/java/net/jmp/speeches/Main.java
@@ -0,0 +1,173 @@
+package net.jmp.speeches;
+
+/*
+ * (#)Main.java 0.1.0   07/05/2025
+ *
+ * @author   Jonathan Parker
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import ch.qos.logback.classic.Level;
+
+import static net.jmp.util.logging.LoggerUtils.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/// The main application class.
+///
+/// @version    0.1.0
+/// @since      0.1.0
+public final class Main implements Runnable {
+    /// The logger.
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    /// Any command line arguments.
+    private final String[] args;
+
+    /// The constructor.
+    ///
+    /// @param  args java.lang.String[]
+    private Main(final String[] args) {
+        super();
+
+        this.args = args;
+    }
+
+    /// The run method.
+    @Override
+    public void run() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        this.logger.info("Speeches");
+
+        this.handleCommandLineArguments();
+
+        final String operation = System.getProperty("app.operation");
+
+        final String chatModel = System.getProperty("app.chatModel");
+        final String mongoDbCollection = System.getProperty("app.mongoDbCollection");
+        final String mongoDbName = System.getProperty("app.mongoDbName");
+        final String mongoDbUriFile = System.getProperty("app.mongoDbUri");
+        final String namespace = System.getProperty("app.namespace");
+        final String rerankingModel = System.getProperty("app.rerankingModel");
+        final String queryText = System.getProperty("app.queryText");
+        final String searchableEmbeddingModel = System.getProperty("app.searchableEmbeddingModel");
+        final String searchableIndexName = System.getProperty("app.searchableIndexName");
+        final String speechesLocation = System.getProperty("app.speechesLocation");
+        final String topK = System.getProperty("app.topK");
+
+        this.logger.info("Operation                 : {}", operation);
+        this.logger.info("Chat Model                : {}", chatModel);
+        this.logger.info("MongoDB Collection        : {}", mongoDbCollection);
+        this.logger.info("MongoDB Name              : {}", mongoDbName);
+        this.logger.info("MongoDB URI File          : {}", mongoDbUriFile);
+        this.logger.info("Namespace                 : {}", namespace);
+        this.logger.info("Reranking Model           : {}", rerankingModel);
+        this.logger.info("Query Text                : {}", queryText);
+        this.logger.info("Searchable Embedding Model: {}", searchableEmbeddingModel);
+        this.logger.info("Searchable Index Name     : {}", searchableIndexName);
+        this.logger.info("Speeches Location         : {}", speechesLocation);
+        this.logger.info("TopK                      : {}", topK);
+
+        final Speeches speeches = Speeches.builder()
+                .chatModel(chatModel)
+                .searchableEmbeddingModel(searchableEmbeddingModel)
+                .searchableIndexName(searchableIndexName)
+                .mongoDbCollection(mongoDbCollection)
+                .mongoDbName(mongoDbName)
+                .mongoDbUriFile(mongoDbUriFile)
+                .namespace(namespace)
+                .rerankingModel(rerankingModel)
+                .queryText(queryText)
+                .speechesLocation(speechesLocation)
+                .topK(Integer.parseInt(topK))
+                .build();
+
+        speeches.start(operation);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Handle any command line arguments.
+    private void handleCommandLineArguments() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        for (final String arg : this.args) {
+            this.logger.info("Command line argument : {}", arg);
+
+            switch (arg) {
+                case "--log-debug": this.setLogLevel(Level.DEBUG); break;
+                case "--log-error": this.setLogLevel(Level.ERROR); break;
+                case "--log-info": this.setLogLevel(Level.INFO); break;
+                case "--log-off": this.setLogLevel(Level.OFF); break;
+                case "--log-trace": this.setLogLevel(Level.TRACE); break;
+                case "--log-warn": this.setLogLevel(Level.WARN); break;
+                default: throw new IllegalArgumentException("Unknown argument: " + arg);
+            }
+        }
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Set the log level.
+    ///
+    /// @param  level   ch.qos.logback.classic.Level
+    private void setLogLevel(final Level level) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(level));
+        }
+
+        final Class<?> clazz = this.getClass();
+        final String packageName = clazz.getPackage().getName();
+        final Logger packageLogger = LoggerFactory.getLogger(packageName);
+
+        /* Get the Logback logger and change it to the new level */
+
+        ch.qos.logback.classic.Logger logbackLogger = (ch.qos.logback.classic.Logger) packageLogger;
+
+        logbackLogger.setLevel(level);
+
+        this.logger.info("{} level logging enabled for package: {}", level.levelStr, packageName);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// The main application entry point.
+    ///
+    /// @param  args    java.lang.String[]
+    public static void main(String[] args) {
+        new Main(args).run();
+    }
+}
---
+++ src/main/java/net/jmp/speeches/Speeches.java
@@ -0,0 +1,476 @@
+package net.jmp.speeches;
+
+/*
+ * (#)Speeches.java 0.1.0   07/05/2025
+ *
+ * @author   Jonathan Parker
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import io.pinecone.clients.Pinecone;
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.*;
+
+import static net.jmp.util.logging.LoggerUtils.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/// The speeches class.
+///
+/// @version    0.1.0
+/// @since      0.1.0
+final class Speeches {
+    /// The logger.
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    /// The chat model.
+    private final String chatModel;
+
+    /// The searchable embedding model.
+    private final String searchableEmbeddingModel;
+
+    /// The searchable index name.
+    private final String searchableIndexName;
+
+    /// The MongoDB collection.
+    private final String mongoDbCollection;
+
+    /// The MongoDB name.
+    private final String mongoDbName;
+
+    /// The MongoDB URI file name.
+    private final String mongoDbUriFile;
+
+    /// The namespace.
+    private final String namespace;
+
+    /// The reranking model.
+    private final String rerankingModel;
+
+    /// The query text.
+    private final String queryText;
+
+    /// The Open AI API key.
+    private String openAiApiKey;
+
+    /// The location of the speeches.
+    private final String speechesLocation;
+
+    /// The number of top results to return when querying.
+    private final int topK;
+
+    /// The constructor.
+    ///
+    /// @param  builder net.jmp.speeches.Speeches.Builder
+    private Speeches(final Builder builder) {
+        super();
+
+        this.chatModel = builder.chatModel;
+        this.searchableEmbeddingModel = builder.searchableEmbeddingModel;
+        this.searchableIndexName = builder.searchableIndexName;
+        this.mongoDbCollection = builder.mongoDbCollection;
+        this.mongoDbName = builder.mongoDbName;
+        this.mongoDbUriFile = builder.mongoDbUriFile;
+        this.namespace = builder.namespace;
+        this.rerankingModel = builder.rerankingModel;
+        this.queryText = builder.queryText;
+        this.speechesLocation = builder.speechesLocation;
+        this.topK = builder.topK;
+    }
+
+    /// The builder method.
+    ///
+    /// @return net.jmp.speeches.Speeches.Builder
+    static Builder builder() {
+        return new Builder();
+    }
+
+    /// The start method.
+    ///
+    /// @param  operation   java.lang.String
+    void start(final String operation) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(operation));
+        }
+
+        this.openAiApiKey = this.getOpenAIApiKey().orElseThrow(() -> new RuntimeException("OpenAI API key not found"));
+
+        final String mongoDbUri = this.getMongoDbUri().orElseThrow(() -> new RuntimeException("MongoDB URI not found"));
+        final String pineconeApiKey = this.getPineconeApiKey().orElseThrow(() -> new RuntimeException("Pinecone API key not found"));
+        final Pinecone pinecone = new Pinecone.Builder(pineconeApiKey).build();
+
+        try (final MongoClient mongoClient = MongoClients.create(mongoDbUri)) {
+            switch (operation) {
+                case "create" -> this.create(pinecone);
+                case "delete" -> this.delete(pinecone);
+                case "load" -> this.load(pinecone, mongoClient);
+                case "search" -> this.search(pinecone);
+                case "store" -> this.store(mongoClient);
+                default -> this.logger.error("Unknown operation: {}", operation);
+            }
+        }
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Create the Pinecone index.
+    ///
+    /// @param  pinecone io.pinecone.clients.Pinecone
+    private void create(final Pinecone pinecone) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(pinecone));
+        }
+
+        this.logger.info("Creating Pinecone index: {}", this.searchableIndexName);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Delete the Pinecone index.
+    ///
+    /// @param  pinecone io.pinecone.clients.Pinecone
+    private void delete(final Pinecone pinecone) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(pinecone));
+        }
+
+        this.logger.info("Deleting Pinecone index: {}", this.searchableIndexName);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Load the speeches from MongoDB into Pinecone.
+    ///
+    /// @param  pinecone io.pinecone.clients.Pinecone
+    /// @param  mongoClient org.mongodb.mongo.MongoClient
+    private void load(final Pinecone pinecone, final MongoClient mongoClient) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(pinecone, mongoClient));
+        }
+
+        this.logger.info("Loading Pinecone index: {}", this.searchableIndexName);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Search the Pinecone index.
+    ///
+    /// @param  pinecone io.pinecone.clients.Pinecone
+    private void search(final Pinecone pinecone) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(pinecone));
+        }
+
+        this.logger.info("Searching Pinecone index: {}", this.searchableIndexName);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Store the speeches into MongoDB.
+    ///
+    /// @param  mongoClient org.mongodb.mongo.MongoClient
+    private void store(final MongoClient mongoClient) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(mongoClient));
+        }
+
+        this.logger.info("Storing speeches: {}", this.speechesLocation);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Get the OpenAI API key.
+    ///
+    /// @return java.util.Optional<java.lang.String>
+    private Optional<String> getOpenAIApiKey() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        final Optional<String> apiKey = this.getApiKey("app.openaiApiKey");
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exitWith(apiKey));
+        }
+
+        return apiKey;
+    }
+
+    /// Get the Pinecone API key.
+    ///
+    /// @return java.util.Optional<java.lang.String>
+    private Optional<String> getPineconeApiKey() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        final Optional<String> apiKey = this.getApiKey("app.pineconeApiKey");
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exitWith(apiKey));
+        }
+
+        return apiKey;
+    }
+
+    /// Get the API key.
+    ///
+    /// @param  propertyName    java.lang.String
+    /// @return                 java.util.Optional<java.lang.String>
+    private Optional<String> getApiKey(final String propertyName) {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(propertyName));
+        }
+
+        final String apiKeyFileName = System.getProperty(propertyName);
+
+        String apiKey = null;
+
+        try {
+            apiKey = Files.readString(Paths.get(apiKeyFileName)).trim();
+
+            if (this.logger.isDebugEnabled()) {
+                this.logger.debug("API key file: {}", apiKeyFileName);
+                this.logger.debug("API key: {}", apiKey);
+            }
+        } catch (final IOException ioe) {
+            this.logger.error("Unable to read API key file: {}", apiKeyFileName, ioe);
+        }
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exitWith(apiKey));
+        }
+
+        return Optional.ofNullable(apiKey);
+    }
+
+    /// Get the MongoDB URI.
+    ///
+    /// @return java.util.Optional<java.lang.String>
+    private Optional<String> getMongoDbUri() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        final String mongoDbUriFileName = this.mongoDbUriFile;
+
+        String mongoDbUri = null;
+
+        try {
+            mongoDbUri = Files.readString(Paths.get(mongoDbUriFileName)).trim();
+
+            if (this.logger.isDebugEnabled()) {
+                this.logger.debug("MongoDb URI file: {}", mongoDbUriFileName);
+                this.logger.debug("MongoDb URI: {}", mongoDbUri);
+            }
+        } catch (final IOException ioe) {
+            this.logger.error("Unable to read MongoDb URI file: {}", mongoDbUriFileName, ioe);
+        }
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exitWith(mongoDbUri));
+        }
+
+        return Optional.ofNullable(mongoDbUri);
+    }
+
+    /// The builder class.
+    static class Builder {
+        /// The chat model.
+        private String chatModel;
+
+        /// The searchable embedding model.
+        private String searchableEmbeddingModel;
+
+        /// The searchable index name.
+        private String searchableIndexName;
+
+        /// The MongoDb collection.
+        private String mongoDbCollection;
+
+        /// The MongoDb name.
+        private String mongoDbName;
+
+        /// The MongoDb URI file name.
+        private String mongoDbUriFile;
+
+        /// The namespace.
+        private String namespace;
+
+        /// The reranking model.
+        private String rerankingModel;
+
+        /// The query text.
+        private String queryText;
+
+        /// The speeches location.
+        private String speechesLocation;
+
+        /// The number of top results to return when querying.
+        private int topK;
+
+        /// The default constructor.
+        Builder() {
+            super();
+        }
+
+        /// Set the chat model.
+        ///
+        /// @param  chatModel   java.lang.String
+        /// @return             net.jmp.speeches.Speeches.Builder
+        public Builder chatModel(final String chatModel) {
+            this.chatModel = chatModel;
+
+            return this;
+        }
+
+        /// Set the searchable embedding model.
+        ///
+        /// @param  searchableEmbeddingModel    java.lang.String
+        /// @return                             net.jmp.speeches.Speeches.Builder
+        public Builder searchableEmbeddingModel(final String searchableEmbeddingModel) {
+            this.searchableEmbeddingModel = searchableEmbeddingModel;
+
+            return this;
+        }
+
+        /// Set the searchable index name.
+        ///
+        /// @param  searchableIndexName java.lang.String
+        /// @return                     net.jmp.speeches.Speeches.Builder
+        public Builder searchableIndexName(final String searchableIndexName) {
+            this.searchableIndexName = searchableIndexName;
+
+            return this;
+        }
+        
+        /// Set the MongoDb collection.
+        ///
+        /// @param  mongoDbCollection   java.lang.String
+        /// @return                     net.jmp.speeches.Speeches.Builder
+        public Builder mongoDbCollection(final String mongoDbCollection) {
+            this.mongoDbCollection = mongoDbCollection;
+
+            return this;
+        }
+
+        /// Set the MongoDb name.
+        ///
+        /// @param  mongoDbName java.lang.String
+        /// @return             net.jmp.speeches.Speeches.Builder
+        public Builder mongoDbName(final String mongoDbName) {
+            this.mongoDbName = mongoDbName;
+
+            return this;
+        }
+
+        /// Set the MongoDb URI.
+        ///
+        /// @param  mongoDbUriFile  java.lang.String
+        /// @return                 net.jmp.speeches.Speeches.Builder
+        public Builder mongoDbUriFile(final String mongoDbUriFile) {
+            this.mongoDbUriFile = mongoDbUriFile;
+
+            return this;
+        }
+
+        /// Set the namespace.
+        ///
+        /// @param  namespace   java.lang.String
+        /// @return             net.jmp.speeches.Speeches.Builder
+        public Builder namespace(final String namespace) {
+            this.namespace = namespace;
+
+            return this;
+        }
+
+        /// Set the reranking model.
+        ///
+        /// @param  rerankingModel  java.lang.String
+        /// @return                 net.jmp.speeches.Speeches.Builder
+        public Builder rerankingModel(final String rerankingModel) {
+            this.rerankingModel = rerankingModel;
+
+            return this;
+        }
+
+        /// Set the query text.
+        ///
+        /// @param  queryText   java.lang.String
+        /// @return             net.jmp.speeches.Speeches.Builder
+        public Builder queryText(final String queryText) {
+            this.queryText = queryText;
+
+            return this;
+        }
+
+        /// Set the speeches location.
+        ///
+        /// @param  speechesLocation    java.lang.String
+        /// @return                     net.jmp.speeches.Speeches.Builder
+        public Builder speechesLocation(final String speechesLocation) {
+            this.speechesLocation = speechesLocation;
+
+            return this;
+        }
+
+        /// Set the topK value.
+        ///
+        /// @param  topK    int
+        /// @return         net.jmp.speeches.Speeches.Builder
+        public Builder topK(final int topK) {
+            this.topK = topK;
+
+            return this;
+        }
+
+        /// Build the speeches object.
+        ///
+        /// @return net.jmp.speeches.Speeches
+        public Speeches build() {
+            return new Speeches(this);
+        }
+    }
+}
